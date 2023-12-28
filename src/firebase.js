2aae67e59b39b92c1bd0dc72786adc47dd544c5a
Usama <fa18-bcs-083@cuilahore.edu.pk>
2023-12-28 22:52:01 +0500

    changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  apiKey: "AIzaSyCm-hcPxkiyc7zzjyr0Wm9GBxOiqZIqp-k",
+  authDomain: "mern-real-estate-8a6a1.firebaseapp.com",
+  projectId: "mern-real-estate-8a6a1",
+  storageBucket: "mern-real-estate-8a6a1.appspot.com",
+  messagingSenderId: "414987423278",
+  appId: "1:414987423278:web:aa95a9f2a741fa77b959bf",
+  measurementId: "G-XHW7XHZEK6"
 };
 
 // Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
